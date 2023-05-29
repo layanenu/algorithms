ad56033e6ca39c21e66f06957c427b7ce92da9bd
Layane Nunes <layanenu@gmail.com>
2023-05-29 14:59:11 -0300

    feat: study_schedule and is_palindrome_recursive

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,11 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not isinstance(target_time, int):
+        return None
+
+    count = 0
+    for entry_time, exit_time in permanence_period:
+        if type(entry_time) != int or type(exit_time) != int:
+            return None
+        if entry_time <= target_time <= exit_time:
+            count += 1
+    return count
